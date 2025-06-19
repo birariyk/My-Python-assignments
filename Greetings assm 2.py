ed50ec4fdf6373bb6591d5185c6f0f920d6dcaa0
birariyk <birari.yogesh@outlook.com>
2025-06-19 11:15:14 +0530

    Add files via upload

---
+++ Greetings assm 2.py
@@ -0,0 +1,8 @@
+#This program takes the name from the user and greets them
+a=input("Please enter your first name :")
+b=input("Please enter your last name :")
+a=str(a)
+b=str(b)
+
+print("Hello ",a," ",b,"! , Welcome to the python program.")
+
