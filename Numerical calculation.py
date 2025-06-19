45f83c9b9dc5db42e24091ecc50a811415b49b52
birariyk <birari.yogesh@outlook.com>
2025-06-19 11:00:28 +0530

    Add files via upload

---
+++ Numerical calculation.py
@@ -0,0 +1,21 @@
+#Simple Interest Calculator
+
+x=input("Enter first number: ")
+y=input("Enter second number: ")
+x=int(x)
+y=int(y)
+a=x+y
+a=int(a)
+s=x-y
+s=int(s)
+m=x*y
+m=int(m)
+d=x/y
+d=int(d)
+
+print("Addition :",a)
+print("Subtraction : ",s)
+print("Multiplication : ",m)
+print("Division : ", d)
+
+
